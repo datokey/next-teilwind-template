b5ea02ca07d588c079ec54520e9d07ef4d064090
datokey <datokey11@gmail.com>
2021-03-27 18:01:37 +0700

    make layout

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-   <div className="flex min-h-screen justify-center items-center bg-red-900 text-xl  ">test</div>
+   <div>test</div>
   )
 }
  
